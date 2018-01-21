16a8a4d7f2df2fe2df9299e608fa035059013bf6
Marc Levine <marc@3sys.com>
2018-01-21 10:20:48 -0600

    added bigoven.com

--- SiteLibrary.py
+++ SiteLibrary.py
@@ -12,12 +12,13 @@ import time
 
 
 sites = ['https://pinchofyum.com', 'http://rachlmansfield.com','https://www.101cookbooks.com','http://12tomatoes.com','http://allrecipes.com','https://www.americastestkitchen.com','https://www.bbc.co.uk/food/recipes/']
-sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com'])
+sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com','https://www.bigoven.com'])
 
 #sites to pull from the print option as this should make the scraping faster
 printSiteList = ['pinchofyum.com','rachlmansfield.com','allrecipes.com','www.bhg.com']
 #sites either without a print option or poorly formated print pages
-noPrintSiteList = ['www.101cookbooks.com','12tomatoes.com','www.americastestkitchen.com','www.bbc.co.uk','www.bbcgoodfood.com']
+noPrintSiteList = ['www.101cookbooks.com','12tomatoes.com','www.americastestkitchen.com','www.bbc.co.uk','www.bbcgoodfood.com','www.bigoven.com']
+noPrintSiteList.extend(['www.bigoven.com'])
 
 db = MySQLdb.connect(host="localhost", user="root", passwd="helifino", db="recipefinder", charset='utf8', use_unicode=True)
 cursor= db.cursor()
@@ -438,19 +439,19 @@ def contentScraper(url):
             # title
             title = soup.find('meta', property='og:title')
             title = title["content"]
-            print(title)
+            #print(title)
             # link
             link = soup.find('meta', property='og:url')
             link = link["content"]
-            print(link)
+            #print(link)
             # image link
             img = soup.find('meta', property='og:image')
             imglink = img["content"]
-            print(imglink)
+            #print(imglink)
             # description
             descr = soup.find('meta', property="og:description")
             descr = descr["content"]
-            print(descr)
+            #print(descr)
             # date no date published
             datePosted = time.strftime('%Y-%m-%d %H:%M:%S')
             # ingredients
@@ -459,7 +460,7 @@ def contentScraper(url):
             ingdiv = soup.find('ul', {"class": "ingredients-list__group"})
             for li in ingdiv.find_all('li'):
                 ing = ing + li.text + "\n"
-            print(ing)
+            #print(ing)
 
             # instructions
             instr = ""
@@ -468,7 +469,7 @@ def contentScraper(url):
                 for p in li.find_all('p'):
                     # prints the p tag content
                     instr = instr + p.text + "\n"
-            print(instr)
+            #print(instr)
             if ing:
                 print("title:%s" % title)  # print title
                 print("Date published %s" % datePosted)
@@ -489,24 +490,24 @@ def contentScraper(url):
             # title
             title = soup.find('meta', property='og:title')
             title = title["content"]
-            print(title)
+            #print(title)
             # link
             link = soup.find('meta', property='og:url')
             link = link["content"]
-            print(link)
+            #print(link)
             # image link
             img = soup.find('meta', property='og:image')
             imglink = img["content"]
-            print(imglink)
+            #print(imglink)
             # description
             descr = soup.find('meta', property="og:description")
             descr = descr["content"]
-            print(descr)
-            # date no date published
+            #print(descr)
+            # date
             d = soup.find('meta', property='bt:pubDate')
             datePosted = d["content"].replace("T"," ")
             datePosted = datePosted[:-6]
-            print(datePosted)
+            #print(datePosted)
 
             # ingredients
             ing = ""
@@ -516,14 +517,65 @@ def contentScraper(url):
                 for txt in li.find_all('span'):
                     ing = ing + txt.text + " "
                 ing = ing + "\n"
-            print(ing)
+            #print(ing)
 
             # instructions
             instr = ""
             instrdiv = soup.find('ol', {"class": "recipe__directionsList"})
             for li in instrdiv.find_all('li'):
                 instr = instr + li.text + "\n"
-            print(instr)
+            #print(instr)
+
+            if ing:
+                print("title:%s" % title)  # print title
+                print("Date published %s" % datePosted)
+                print("Url: %s" % link)
+                print("Description: %s" % descr)
+                print("imglink: %s" % imglink)  # print image source
+                print("ingredients: %s" % ing)  # print ingredients
+                print("instructions: %s" % instr)
+                add_recipe = "insert into recipes (title, link, ingredients, description, image ,dateposted, instructions) values (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(add_recipe, (title, link, ing, descr, imglink, datePosted, instr))
+                db.commit()
+
+        except:
+            print("no recipe found @ %s" % url)
+
+#bigoven.com
+    if "bigoven.com" in url:
+        try:
+            # title
+            title = soup.find('meta', property='og:title')
+            title = title["content"]
+            #print(title)
+            # link
+            link = soup.find('meta', property='og:url')
+            link = link["content"]
+            #print(link)
+            # image link
+            img = soup.find('meta', property='og:image')
+            imglink = img["content"]
+            #print(imglink)
+            # description
+            descr = soup.find('meta', property="og:description")
+            descr = descr["content"]
+            #print(descr)
+            # date no date published
+            datePosted = time.strftime('%Y-%m-%d %H:%M:%S')
+
+            # ingredients
+            ing = ""
+            # ingredients
+            ingdiv = soup.find('textarea', id='recipe-textarea')
+            ing = ingdiv.text
+            #print(ing)
+
+            # instructions
+            instr = ""
+            instrdiv = soup.find('div', {"class": "recipe-instructions"})
+            for p in instrdiv.find_all('p'):
+                instr = instr + p.text.strip() + "\n"
+            #print(instr)
 
             if ing:
                 print("title:%s" % title)  # print title
