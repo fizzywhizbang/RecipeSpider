b4c3e495434a985f33626f0153725971790e671c
Marc Levine <marc@3sys.com>
2018-01-22 17:52:11 -0600

    added site and updated code
    added foodnetwork.com and fixed the site validation to keep from having duplicates in the database.

--- spider.py
+++ spider.py
@@ -5,12 +5,12 @@ from six.moves.urllib.request import urlopen, Request
 import requests
 from bs4 import BeautifulSoup
 import sys
-import MySQLdb
 import random
 from scrapme import getProxy
 from SiteLibrary import contentScraper
 from SiteLibrary import printSiteList
 from SiteLibrary import noPrintSiteList
+from SiteLibrary import urlValidate
 from fake_useragent import UserAgent
 from time import sleep
 
@@ -21,10 +21,6 @@ hdr = {
     'Accept-Encoding': 'none',
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'}
-
-
-db = MySQLdb.connect(host="localhost", user="root", passwd="helifino", db="recipefinder", charset='utf8', use_unicode=True)
-cursor= db.cursor()
 
 class RecursiveScraper:
     ''' Scrape URLs in a recursive manner.
@@ -61,12 +57,6 @@ class RecursiveScraper:
                 return cleanurl
 
         return None
-    def urlValidate(self,url=None):
-        query="select * from recipes where link=%s"
-        cursor.execute(query, [url])
-        result = cursor.fetchone()
-        number_of_rows = result[0]
-        return number_of_rows
 
     def scrape(self, url=None):
         ''' Scrape the URL and its outward links in a depth-first order.
@@ -75,21 +65,23 @@ class RecursiveScraper:
 
         if url is None:
             url = self.mainurl
-        print("checking %s" % url)
         if self.domain in printSiteList:
             if "print" in url:
                 print("Scraping {:s} ...".format(url))
                 #check if url exists in database
-                #if self.urlValidate(url) == 0:
-                contentScraper(url, self.domain)
-                #else:
-                #    print("%s exists" % url)
+                if urlValidate(url) == 0:
+                    contentScraper(url, self.domain)
+                else:
+                    print("%s exists" % url)
+
 
         if self.domain in noPrintSiteList and ".rdf" not in url and ".rss" not in url and "print" not in url: #skip news feeds and sites with poorly formated print pages
             print("Scraping {:s} ...".format(url))
             # check if url exists in database
-            #if self.urlValidate(url) == 0:
-            contentScraper(url, self.domain)
+            if urlValidate(url) == 0:
+                contentScraper(url, self.domain)
+            else:
+                print("%s exists" % url)
 
         self.urls.add(url)
 
