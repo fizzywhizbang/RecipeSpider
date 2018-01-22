3b86c30ff65225054a27294a4761623e2acf29b9
Marc Levine <marc@3sys.com>
2018-01-22 17:00:03 -0600

    added food.com/geniuskitchen.com

--- SiteLibrary.py
+++ SiteLibrary.py
@@ -15,13 +15,13 @@ import datetime
 sites = ['https://pinchofyum.com', 'http://rachlmansfield.com','https://www.101cookbooks.com','http://12tomatoes.com','http://allrecipes.com','https://www.americastestkitchen.com','https://www.bbc.co.uk/food/recipes/']
 sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com','https://www.bigoven.com','https://www.bonappetit.com','https://www.chowhound.com','http://www.cookingchanneltv.com','https://cooking.nytimes.com'])
 sites.extend(['http://www.cooks.com','https://www.cooksillustrated.com','https://www.dadcooksdinner.com','http://www.eatingwell.com','https://elanaspantry.com','https://www.epicurious.com'])
-sites.extend(['https://food52.com','http://www.foodandwine.com'])
+sites.extend(['https://food52.com','http://www.foodandwine.com','http://www.geniuskitchen.com'])
 #sites to pull from the print option as this should make the scraping faster
 printSiteList = ['rachlmansfield.com','www.bhg.com']
 #sites either without a print option or poorly formated print pages
 noPrintSiteList = ['allrecipes.com','pinchofyum.com','www.101cookbooks.com','12tomatoes.com','www.americastestkitchen.com','www.bbc.co.uk','www.bbcgoodfood.com','www.bigoven.com']
 noPrintSiteList.extend(['www.bigoven.com','www.bonappetit.com','www.chowhound.com','www.cookingchanneltv.com','cooking.nytimes.com','www.cooks.com','www.cooksillustrated.com'])
-noPrintSiteList.extend(['www.dadcooksdinner.com','www.eatingwell.com','elanaspantry.com','www.epicurious.com','food52.com','www.foodandwine.com'])
+noPrintSiteList.extend(['www.dadcooksdinner.com','www.eatingwell.com','elanaspantry.com','www.epicurious.com','food52.com','www.foodandwine.com','www.geniuskitchen.com'])
 db = MySQLdb.connect(host="localhost", user="root", passwd="helifino", db="recipefinder", charset='utf8', use_unicode=True)
 cursor= db.cursor()
 proxy = getProxy()
@@ -1244,6 +1244,54 @@ def contentScraper(url, domain):
             # instructions
             instr = data['recipeInstructions']
             #print(instr)
+
+            if ing:
+                print("title:%s" % title)  # print title
+                print("Date published %s" % datePosted)
+                print("Url: %s" % link)
+                print("Description: %s" % descr)
+                print("imglink: %s" % imglink)  # print image source
+                print("ingredients: %s" % ing)  # print ingredients
+                print("instructions: %s" % instr)
+                add_recipe = "insert into recipes (title, link, ingredients, description, image ,dateposted, instructions, jsondata) values (%s, %s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(add_recipe, (title, link, ing, descr, imglink, datePosted, instr, rawdata))
+                db.commit()
+
+        except:
+            print("no recipe found @ %s" % url)
+
+#food.com / http://www.geniuskitchen.com
+    if "geniuskitchen.com" in url:
+        try:
+            # json and html scraping since the instructions are not in the json
+            rawdata = soup.find('script', type='application/ld+json').text
+            data = json.loads(soup.find('script', type='application/ld+json').text)
+            #data = data[0]
+            # title
+            title = data['name']
+            print(title)
+            # link
+            link = url
+            print(link)
+            # image link
+            imglink = data['image']
+            print(imglink)
+            # description
+            descr = data['description']
+            print(descr)
+            # date published
+            datePosted = time.strftime('%Y-%m-%d %H:%M:%S')
+            # ingredients loop through the ingredient list
+            ing = ""
+            ingList = data['recipeIngredient']
+            i = 0
+            while i < len(ingList):
+                ing += ingList[i].strip() + "\n"
+                i += 1
+            print(ing)
+            # instructions
+            instr = data['recipeInstructions']
+            print(instr)
 
             if ing:
                 print("title:%s" % title)  # print title
