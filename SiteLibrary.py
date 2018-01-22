1562f64586e320cf640a46855e7bfab6afb27fc3
Marc Levine <marc@3sys.com>
2018-01-22 05:45:20 -0600

    added bonappetit.com

--- SiteLibrary.py
+++ SiteLibrary.py
@@ -9,16 +9,16 @@ import random
 from scrapme import getProxy
 from fake_useragent import UserAgent
 import time
-
+import json
 
 sites = ['https://pinchofyum.com', 'http://rachlmansfield.com','https://www.101cookbooks.com','http://12tomatoes.com','http://allrecipes.com','https://www.americastestkitchen.com','https://www.bbc.co.uk/food/recipes/']
-sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com','https://www.bigoven.com'])
+sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com','https://www.bigoven.com','https://www.bonappetit.com'])
 
 #sites to pull from the print option as this should make the scraping faster
 printSiteList = ['pinchofyum.com','rachlmansfield.com','allrecipes.com','www.bhg.com']
 #sites either without a print option or poorly formated print pages
 noPrintSiteList = ['www.101cookbooks.com','12tomatoes.com','www.americastestkitchen.com','www.bbc.co.uk','www.bbcgoodfood.com','www.bigoven.com']
-noPrintSiteList.extend(['www.bigoven.com'])
+noPrintSiteList.extend(['www.bigoven.com','www.bonappetit.com'])
 
 db = MySQLdb.connect(host="localhost", user="root", passwd="helifino", db="recipefinder", charset='utf8', use_unicode=True)
 cursor= db.cursor()
@@ -230,7 +230,7 @@ def contentScraper(url):
             d = soup.find('meta', property='article:published_time')
             datePosted = d["content"]
             datePosted = datePosted.replace("T"," ")
-            datePosted = datePosted.replace("+00:00","")
+            datePosted = datePosted[:-6]
             # image link
             img = soup.find('meta', property='og:image')
             imglink = img["content"]
@@ -578,6 +578,59 @@ def contentScraper(url):
             #print(instr)
 
             if ing:
+                print("title:%s" % title)  # print title
+                print("Date published %s" % datePosted)
+                print("Url: %s" % link)
+                print("Description: %s" % descr)
+                print("imglink: %s" % imglink)  # print image source
+                print("ingredients: %s" % ing)  # print ingredients
+                print("instructions: %s" % instr)
+                add_recipe = "insert into recipes (title, link, ingredients, description, image ,dateposted, instructions) values (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(add_recipe, (title, link, ing, descr, imglink, datePosted, instr))
+                db.commit()
+
+        except:
+            print("no recipe found @ %s" % url)
+#bonappetit.com
+    if "bonappetit.com" in url:
+        try:
+            #json and html scraping since the instructions are not in the json
+            type = "json"
+            data = json.loads(soup.find('script', type='application/ld+json').text)
+            #title
+            title = data['name']
+            #print(title)
+            # link
+            link = soup.find('meta', property='og:url')
+            link = link["content"]
+            #image link
+            imglink = data['image']
+            #print(imglink)
+            #description
+            descr = data['description']
+            #print(descr)
+            #date published
+            d = data['datePublished']
+            datePosted = d.replace("T", " ")[:-10]
+
+            #print(datePosted)
+            #ingredients loop through the ingredient list
+            ing = ""
+            ingList = data['recipeIngredient']
+            i = 0
+            while i < len(ingList):
+                ing += ingList[i] + "\n"
+                i += 1
+            #print(ing)
+            #instructions
+            instrdiv = soup.find("ul", {'class': 'steps'})
+            instr = ""
+            for li in instrdiv.find_all('li'):
+                instr = instr + li.text + "\n"
+            #print(instr)
+
+            if ing:
+                print(type)
                 print("title:%s" % title)  # print title
                 print("Date published %s" % datePosted)
                 print("Url: %s" % link)
