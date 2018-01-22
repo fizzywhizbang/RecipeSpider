7724b923b9d7b19204db6999c6756b17965f4cf0
Marc Levine <marc@3sys.com>
2018-01-22 11:37:54 -0600

    added dadcooksdinner.com

--- SiteLibrary.py
+++ SiteLibrary.py
@@ -14,13 +14,13 @@ import datetime
 
 sites = ['https://pinchofyum.com', 'http://rachlmansfield.com','https://www.101cookbooks.com','http://12tomatoes.com','http://allrecipes.com','https://www.americastestkitchen.com','https://www.bbc.co.uk/food/recipes/']
 sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com','https://www.bigoven.com','https://www.bonappetit.com','https://www.chowhound.com','http://www.cookingchanneltv.com','https://cooking.nytimes.com'])
-sites.extend(['http://www.cooks.com','https://www.cooksillustrated.com'])
+sites.extend(['http://www.cooks.com','https://www.cooksillustrated.com','https://www.dadcooksdinner.com'])
 #sites to pull from the print option as this should make the scraping faster
 printSiteList = ['rachlmansfield.com','www.bhg.com']
 #sites either without a print option or poorly formated print pages
 noPrintSiteList = ['allrecipes.com','pinchofyum.com','www.101cookbooks.com','12tomatoes.com','www.americastestkitchen.com','www.bbc.co.uk','www.bbcgoodfood.com','www.bigoven.com']
 noPrintSiteList.extend(['www.bigoven.com','www.bonappetit.com','www.chowhound.com','www.cookingchanneltv.com','cooking.nytimes.com','www.cooks.com','www.cooksillustrated.com'])
-
+noPrintSiteList.extend(['www.dadcooksdinner.com'])
 db = MySQLdb.connect(host="localhost", user="root", passwd="helifino", db="recipefinder", charset='utf8', use_unicode=True)
 cursor= db.cursor()
 proxy = getProxy()
@@ -36,6 +36,7 @@ def contentScraper(url, domain):
 
     response = requests.get(url, headers=hdr, proxies=proxy)
     soup = BeautifulSoup(response.content, "html.parser")
+
 # pinch of yum
     if "pinchofyum" in url:
         try:
@@ -944,3 +945,57 @@ def contentScraper(url, domain):
 
        except:
            print("no recipe found @ %s" % url)
+
+#dadscookdinner
+#https://www.dadcooksdinner.com/pressure-cooker-baby-back-ribs/
+    if "dadcooksdinner.com" in url:
+        try:
+            rawdata = soup.find('script', type='application/ld+json').text
+            for recipe in soup.find_all('script', type='application/ld+json'):
+                data = json.loads(recipe.text)
+                if data['@type'] == "Recipe":
+                    # title
+                    title = data['name']
+                    print(title)
+                    # link
+                    link = data["url"]
+                    print(link)
+                    #image link
+                    imglink = data['image']['url']
+                    print(imglink)
+                    #description
+                    descr = data['description']
+                    print(descr)
+                    #date published
+                    datePosted = time.strftime('%Y-%m-%d %H:%M:%S')
+                    # ingredients loop through the ingredient list
+                    ing = ""
+                    ingList = data['recipeIngredient']
+                    i = 0
+                    while i < len(ingList):
+                        ing += ingList[i] + "\n"
+                        i += 1
+                    print(ing)
+                    # instructions
+                    instrList = data['recipeInstructions']
+                    i = 0
+                    instr = ""
+                    while i < len(instrList):
+                        instr += instrList[i] + "\n"
+                        i += 1
+                    print(instr)
+
+                    if ing:
+                        print("title:%s" % title)  # print title
+                        print("Date published %s" % datePosted)
+                        print("Url: %s" % link)
+                        print("Description: %s" % descr)
+                        print("imglink: %s" % imglink)  # print image source
+                        print("ingredients: %s" % ing)  # print ingredients
+                        print("instructions: %s" % instr)
+                        add_recipe = "insert into recipes (title, link, ingredients, description, image ,dateposted, instructions, jsondata) values (%s, %s, %s, %s, %s, %s, %s, %s)"
+                        cursor.execute(add_recipe, (title, link, ing, descr, imglink, datePosted, instr, rawdata))
+                        db.commit()
+
+        except:
+            print("no recipe found @ %s" % url)
