bcb73735f3f52ed4f64965bf863ec0ca3aeaac73
Marc Levine <marc@3sys.com>
2018-01-22 12:11:17 -0600

    added eatingwell.com

--- SiteLibrary.py
+++ SiteLibrary.py
@@ -14,13 +14,13 @@ import datetime
 
 sites = ['https://pinchofyum.com', 'http://rachlmansfield.com','https://www.101cookbooks.com','http://12tomatoes.com','http://allrecipes.com','https://www.americastestkitchen.com','https://www.bbc.co.uk/food/recipes/']
 sites.extend(['https://www.bbcgoodfood.com','https://www.bhg.com','https://www.bigoven.com','https://www.bonappetit.com','https://www.chowhound.com','http://www.cookingchanneltv.com','https://cooking.nytimes.com'])
-sites.extend(['http://www.cooks.com','https://www.cooksillustrated.com','https://www.dadcooksdinner.com'])
+sites.extend(['http://www.cooks.com','https://www.cooksillustrated.com','https://www.dadcooksdinner.com','http://www.eatingwell.com'])
 #sites to pull from the print option as this should make the scraping faster
 printSiteList = ['rachlmansfield.com','www.bhg.com']
 #sites either without a print option or poorly formated print pages
 noPrintSiteList = ['allrecipes.com','pinchofyum.com','www.101cookbooks.com','12tomatoes.com','www.americastestkitchen.com','www.bbc.co.uk','www.bbcgoodfood.com','www.bigoven.com']
 noPrintSiteList.extend(['www.bigoven.com','www.bonappetit.com','www.chowhound.com','www.cookingchanneltv.com','cooking.nytimes.com','www.cooks.com','www.cooksillustrated.com'])
-noPrintSiteList.extend(['www.dadcooksdinner.com'])
+noPrintSiteList.extend(['www.dadcooksdinner.com','www.eatingwell.com'])
 db = MySQLdb.connect(host="localhost", user="root", passwd="helifino", db="recipefinder", charset='utf8', use_unicode=True)
 cursor= db.cursor()
 proxy = getProxy()
@@ -999,3 +999,54 @@ def contentScraper(url, domain):
 
         except:
             print("no recipe found @ %s" % url)
+#eatingwell.com
+    if "eatingwell.com" in url:
+       try:
+            #title
+            title = soup.find("meta", property="og:title")
+            title = title["content"]
+            print(title)
+            # link
+            link = soup.find("meta", property="og:url")
+            link = link["content"]
+            print(link)
+            #image link
+            imglink = soup.find("meta", property="og:image")
+            imglink = imglink["content"]
+            print(imglink)
+            #description
+            descr = soup.find("meta", property="og:description")
+            descr = descr["content"]
+            print(descr)
+            #date published
+            datePosted = time.strftime('%Y-%m-%d %H:%M:%S')
+
+            #ingredients loop through the ingredient list
+            ing = ""
+            ingList = soup.find_all("span", itemprop="ingredients")
+            for span in ingList:
+                ing += span.text + "\n"
+            print(ing)
+
+            #instructions
+            instr = ""
+            instrdiv = soup.find("ol", itemprop="recipeInstructions")
+            for span in instrdiv.find_all("span"):
+                instr += span.text + "\n"
+            print(instr)
+
+            if ing:
+                print(type)
+                print("title:%s" % title)  # print title
+                print("Date published %s" % datePosted)
+                print("Url: %s" % link)
+                print("Description: %s" % descr)
+                print("imglink: %s" % imglink)  # print image source
+                print("ingredients: %s" % ing)  # print ingredients
+                print("instructions: %s" % instr)
+                add_recipe = "insert into recipes (title, link, ingredients, description, image ,dateposted, instructions) values (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(add_recipe, (title, link, ing, descr, imglink, datePosted, instr))
+                db.commit()
+
+       except:
+           print("no recipe found @ %s" % url)
